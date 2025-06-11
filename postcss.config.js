80792d46217763fc7aa2b3095957096557cfcc96
saskia <sskiaanggi123@gmail.com>
2025-06-11 21:52:28 +0700

    Fix: use CommonJS for PostCSS config for Netlify compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-export default {
+module.exports = {
   plugins: [
-    tailwindcss(),
-    autoprefixer(),
+    require('tailwindcss'),
+    require('autoprefixer'),
   ],
 };
